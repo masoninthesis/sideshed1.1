c44bdc904f5b8e3c5f4ba7c279766c2674d535fa
Mason L <mason.lawlor@me.com>
2016-04-13 00:34:42 -0600

    Laid base styles for home page

---
+++ site/web/app/themes/sage/base-front-page.php
@@ -0,0 +1,48 @@
+<?php
+
+use Roots\Sage\Setup;
+use Roots\Sage\Wrapper;
+
+?>
+
+<!doctype html>
+<html <?php language_attributes(); ?>>
+  <?php get_template_part('templates/head'); ?>
+  <body <?php body_class(); ?>>
+    <!--[if IE]>
+      <div class="alert alert-warning">
+        <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
+      </div>
+    <![endif]-->
+    <?php
+      do_action('get_header');
+      get_template_part('templates/header');
+    ?>
+
+    <div class="container">
+      <div class="bg-borders">
+        <div class="column-border col-xs-4"></div>
+        <div class="column-border col-xs-4"></div>
+        <div class="column-border col-xs-4"></div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="cta text-center">
+        <svg class="top-tri" height="18"><?php get_template_part( 'assets/svg/inline', 'doubletris.svg' ); ?></svg>
+        <h2>The</h2>
+        <h2>The</h2>
+        <h1>Story</h1>
+        <svg class="bottom-tri" height="18"><?php get_template_part( 'assets/svg/inline', 'doubletris.svg' ); ?></svg>
+      </div>
+      <svg class="vid-thumb" height="80"><?php get_template_part( 'assets/svg/inline', 'vidthumb.svg' ); ?></svg>
+    </div>
+    <div class="bg-img">
+      <svg class="moonzags" width="800" height="720"><?php get_template_part( 'assets/svg/inline', 'moonzags.svg' ); ?></svg>
+    </div>
+    <div class="container navbar-fixed-bottom footer">
+      <div class="footer-cell">
+        <svg class="clients img-resposive" width="1000" height="100"><?php get_template_part( 'assets/svg/inline', 'clients.svg' ); ?></svg>
+      </div>
+    </div>
+  </body>
+</html>
--- site/web/app/themes/sage/templates/header.php
+++ site/web/app/themes/sage/templates/header.php
@@ -1,12 +1,20 @@
-<header class="banner">
+<header class="banner" id="nav">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
+    <ul class="list-inline">
+      <li><a class="brand" href="<?= esc_url(home_url('/')); ?>"><svg class="nav-logo" height="72" width="133"><?php get_template_part( 'assets/svg/inline', 'logo.svg' ); ?></svg></a></li>
+      <li class="nav-primary"><nav>
+        <!-- <?php
+        if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        endif;
+        ?> -->
+        <ul class="list-inline">
+          <li>Talent</li>
+          <li>Portfolio</li>
+          <li>Services</li>
+          <li>Hire Us</li>
+        </ul>
+      </nav></li>
+    </ul>
   </div>
 </header>
